ed52bc949840842f22d90f2fc8cc7733ecb36e90
Gaurav Agarwal <grv@chromium.org>
2014-06-05 01:19:45 -0700

    Stop propogation of click event on submit in SparkDialog.

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -1171,7 +1171,10 @@ abstract class SparkActionWithDialog extends SparkAction {
     _dialog = spark.createDialog(dialogElement);
     final Element submitBtn = _dialog.getElement("[submit]");
     if (submitBtn != null) {
-      submitBtn.onClick.listen((_) => _commit());
+      submitBtn.onClick.listen((Event e) {
+        e..stopPropagation()..preventDefault();
+        _commit();
+      });
     }
   }
 
