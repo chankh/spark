c22265a019c5ce64282f85169394aa747e5fe6d9
Gaurav Agarwal <grv@chromium.org>
2014-06-08 21:38:36 -0700

    Show folder picker before any create operation in workspace.

--- ide/app/spark.dart
+++ ide/app/spark.dart
@@ -1968,7 +1968,10 @@ class NewProjectAction extends SparkActionWithDialog {
 
   void _invoke([context]) {
     _nameElt.value = '';
-    _show();
+    // Show folder picker, if top-level folder is not set.
+    spark.projectLocationManager.getProjectLocation().then((_) {
+      _show();
+    });
   }
 
   void _commit() {
@@ -2295,8 +2298,10 @@ class GitCloneAction extends SparkActionWithProgressDialog {
   void _invoke([Object context]) {
     // Select any previous text in the URL field.
     Timer.run(_repoUrlElement.select);
-
-    _show();
+    // Show folder picker, if top-level folder is not set.
+    spark.projectLocationManager.getProjectLocation().then((_) {
+      _show();
+    });
   }
 
   void _restoreDialog() {
